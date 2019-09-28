16318ee999532cfa243f848976ecee08edbbf351
Vinicius <vini_carvalho99@hotmail.com>
2019-09-28 13:25:07 -0300

    os blocos podem mudar

---
+++ Game/Assets/Scripts/BlockSM.gd
@@ -0,0 +1,33 @@
+extends Node
+
+onready var sprite = get_node("Sprite")
+
+enum {
+ damage,
+ NoDamage,
+ Intangible
+}
+
+var state =  NoDamage
+var anim = "NoDamage"
+
+func _process(delta):
+ ChangeState()
+ match state:
+  
+  damage:
+   pass
+  NoDamage:
+   pass
+  Intangible:
+   pass
+
+func ChangeState():
+ state = randi()%3
+ if state == damage:
+  anim =  "damage"
+ elif state == NoDamage :
+  anim = "NoDamage"
+ elif state == Intangible:
+  anim = "Intangible"
+ sprite.play(anim)
