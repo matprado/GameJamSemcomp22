47f6ea9e260b5b4b1514b78ac13a0ee8cbb51ca8
Mateus Prado <mateus.prado@usp.br>
2019-09-28 18:25:59 -0300

    VAI

--- Game/Assets/Scripts/Enemy.gd
+++ Game/Assets/Scripts/Enemy.gd
@@ -4,6 +4,15 @@ const GRAVITY = 10
 const SPEED = 25
 const RUN_SPEED = 200
 const FLOOR = Vector2(0, -1)
+const DOG_POSITION_X = -1.1138
+const DOG_POSITION_Y = 2.923
+const DOG_SCALE_X = 1
+const DOG_SCALE_Y = 1
+const GOLEM_POSITION_X = 9.055
+const GOLEM_POSITION_Y = 0.606
+const GOLEM_SCALE_X = 0.65
+const GOLEM_SCALE_Y = 1.2
+
 
 var velocity = Vector2(1, 0)
 var direction = 1
@@ -18,7 +27,7 @@ func _ready():
 	$FlipTimer.wait_time = rand_range(1.0, 5.0)
 	$WalkRunTimer.wait_time = rand_range(1.0, 5.0)
 	$KinematicBody2D/AnimatedSprite.set_flip_h(true)
-	$KinematicBody2D/AnimatedSprite.play("walk");
+	$KinematicBody2D/AnimatedSprite.play("DogWalk");
 	pass # Replace with function body.
 
 func _physics_process(delta):
@@ -43,8 +52,8 @@ func _on_Timer_timeout():
 func _on_WalkRunTimer_timeout():
 	if(is_running):
 		is_running = false
-		$KinematicBody2D/AnimatedSprite.play("walk");
+		$KinematicBody2D/AnimatedSprite.play("DogWalk");
 	else:
 		is_running = true
-		$KinematicBody2D/AnimatedSprite.play("run");	
+		$KinematicBody2D/AnimatedSprite.play("DogRun");	
 	pass # Replace with function body.
