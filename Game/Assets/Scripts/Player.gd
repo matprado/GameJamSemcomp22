6fe7a87a44f694865f29a3c0b394f11f3895222b
Tomieiro <matheustomieiro@usp.br>
2019-09-28 15:54:36 -0300

    Okok

--- Game/Assets/Scripts/Player.gd
+++ Game/Assets/Scripts/Player.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
  
-# Declare member variables here. Examples:
+#variables
 onready var corpus = $Sprite
 onready var animation = $Sprite/anim
 var looking_direction = true
 var alt = 0
+var acceleration = 400;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+#Fuction that swtiches body direction
+func switch_dir():
+	looking_direction = !looking_direction
+	corpus.flip_h = !corpus.flip_h
 
+#Function thats controll every physics on body moviment
 func _physics_process(delta):
 	var direction = 0
 	if Input.is_action_pressed("button_1"):
@@ -18,42 +24,37 @@ func _physics_process(delta):
 	if Input.is_action_pressed("button_2"):
 		direction += 1
     
-	move_and_slide(Vector2(direction * 500, alt), Vector2(0, -1))
+	move_and_slide(Vector2(acceleration*direction, alt), Vector2(0, -1))
    
 	var grounded = is_on_floor()
-	alt += 500
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	alt += 50
+
+	if is_on_floor() and Input.is_action_just_pressed("button_3"):
 		alt = -1000
-	if is_on_floor() and alt >= 10:
-		alt = 10
+
+	if is_on_floor() and alt >= 5:
+		alt = 5
+
 	if alt > 1000:
 		alt = 1000
 
-	if looking_direction and direction < 0:
-		flip()
-	if !looking_direction and direction > 0:
-		 flip()
-   
+	if (looking_direction and direction < 0) or (!looking_direction and direction > 0):
+		switch_dir()
+
 	if is_on_floor():
 		if direction == 0:
-			play_anim("idle")
+			if animation.is_playing() and animation.current_animation == "idle":
+				return
+			animation.play("idle")
 		else:
-			play_anim("walk")
+			if animation.is_playing() and animation.current_animation == "walk":
+				return
+			animation.play("walk")
 	else:
-		play_anim("jump")
+		if animation.is_playing() and animation.current_animation == "jump":
+				return
+		animation.play("jump")
  
-
-func flip():
-	looking_direction = !looking_direction
-	corpus.flip_h = !corpus.flip_h
- 
-func play_anim(anim_name):
-	if animation.is_playing() and animation.current_animation == anim_name:
-		return
-	animation.play(anim_name)
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
