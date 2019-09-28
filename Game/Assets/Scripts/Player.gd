46f6c6d52f3342c839f04f07f650bf777ba920c1
Tomieiro <matheustomieiro@usp.br>
2019-09-28 15:39:18 -0300

    Ok

--- Game/Assets/Scripts/Player.gd
+++ Game/Assets/Scripts/Player.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 onready var corpus = $Sprite
 onready var animation = $Sprite/anim
+var looking_direction = true
+var alt = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,45 +12,45 @@ func _ready():
 
 
 func _physics_process(delta):
-    var direction = 0
+	var direction = 0
+	if Input.is_action_pressed("button_1"):
+		direction -= 1
 	if Input.is_action_pressed("button_2"):
-       direction -= 1
-    if Input.is_action_pressed("button_1"):
-        direction += 1
+		direction += 1
     
-    move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+	move_and_slide(Vector2(direction * 500, alt), Vector2(0, -1))
    
-    var grounded = is_on_floor()
-    y_velo += GRAVITY
-    if grounded and Input.is_action_just_pressed("jump"):
-        y_velo = -JUMP_FORCE
-    if grounded and y_velo >= 5:
-        y_velo = 5
-    if y_velo > MAX_FALL_SPEED:
-        y_velo = MAX_FALL_SPEED
+	var grounded = is_on_floor()
+	alt += 500
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		alt = -1000
+	if is_on_floor() and alt >= 10:
+		alt = 10
+	if alt > 1000:
+		alt = 1000
+
+	if looking_direction and direction < 0:
+		flip()
+	if !looking_direction and direction > 0:
+		 flip()
    
-    if facing_right and move_dir < 0:
-        flip()
-    if !facing_right and move_dir > 0:
-        flip()
-   
-    if grounded:
-        if move_dir == 0:
-            play_anim("idle")
-        else:
-            play_anim("walk")
-    else:
-        play_anim("jump")
+	if is_on_floor():
+		if direction == 0:
+			play_anim("idle")
+		else:
+			play_anim("walk")
+	else:
+		play_anim("jump")
  
 
 func flip():
-    facing_right = !facing_right
-    sprite.flip_h = !sprite.flip_h
+	looking_direction = !looking_direction
+	corpus.flip_h = !corpus.flip_h
  
 func play_anim(anim_name):
-    if anim_player.is_playing() and anim_player.current_animation == anim_name:
-        return
-    anim_player.play(anim_name)
+	if animation.is_playing() and animation.current_animation == anim_name:
+		return
+	animation.play(anim_name)
 
 
 
