8e3190b87a1ce726f587d8a0d5b962635515a3bf
Tomieiro <matheustomieiro@usp.br>
2019-09-28 15:09:53 -0300

    Ok

--- Game/Assets/Scripts/Player.gd
+++ Game/Assets/Scripts/Player.gd
@@ -1,16 +1,8 @@
 extends KinematicBody2D
-
-const MOVE_SPEED = 500
-const JUMP_FORCE = 1000
-const GRAVITY = 50
-const MAX_FALL_SPEED = 1000
-
-var y_velo = 0
-var facing_right = true
  
 # Declare member variables here. Examples:
-onready var sprite = $Sprite
-onready var anim_player = $Sprite/anim
+onready var corpus = $Sprite
+onready var animation = $Sprite/anim
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,11 +10,12 @@ func _ready():
 
 
 func _physics_process(delta):
-    var move_dir = 0
-    if Input.is_action_pressed("move_right"):
-        move_dir += 1
-    if Input.is_action_pressed("move_left"):
-        move_dir -= 1
+    var direction = 0
+	if Input.is_action_pressed("button_2"):
+       direction -= 1
+    if Input.is_action_pressed("button_1"):
+        direction += 1
+    
     move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
    
     var grounded = is_on_floor()
@@ -47,6 +40,7 @@ func _physics_process(delta):
     else:
         play_anim("jump")
  
+
 func flip():
     facing_right = !facing_right
     sprite.flip_h = !sprite.flip_h
