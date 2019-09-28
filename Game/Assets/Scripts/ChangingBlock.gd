910d732e7d77e78901f047e0bc4248c964c1b9d4
Mateus Prado <mateus.prado@usp.br>
2019-09-28 11:31:28 -0300

    Sprites

--- Game/Assets/Scripts/ChangingBlock.gd
+++ Game/Assets/Scripts/ChangingBlock.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum BlockType{
+	SIMPLE,
+	DAMAGE,
+	SLOW	
+}
+
+var currentType;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite.modulate = Color.aqua;
+	currentType = BlockType.SIMPLE;
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if(currentType == BlockType.SIMPLE):
+		
+				
+	pass
