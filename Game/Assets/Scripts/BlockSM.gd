0f909ca192ba2c737bde5004e17527d53cbe967a
Vinicius <vini_carvalho99@hotmail.com>
2019-09-28 13:41:42 -0300

    ...

--- Game/Assets/Scripts/BlockSM.gd
+++ Game/Assets/Scripts/BlockSM.gd
@@ -3,22 +3,17 @@ extends Node
 onready var sprite = get_node("Sprite")
 
 enum {
-<<<<<<< HEAD
+
  damage,
  NoDamage,
  Intangible
-=======
-	damage,
-	NoDamage,
-	Intangible
->>>>>>> 6fdc375c240ba3e138241b592092c3d958f4f2e3
 }
 
 var state =  NoDamage
 var anim = "NoDamage"
 
 func _process(delta):
-<<<<<<< HEAD
+
  ChangeState()
  match state:
   
@@ -38,24 +33,3 @@ func ChangeState():
  elif state == Intangible:
   anim = "Intangible"
  sprite.play(anim)
-=======
-	ChangeState()
-	match state:
-		
-		damage:
-			pass
-		NoDamage:
-			pass
-		Intangible:
-			pass
-
-func ChangeState():
-	state = randi()%3
-	if state == damage:
-		anim =  "damage"
-	elif state == NoDamage :
-		anim = "NoDamage"
-	elif state == Intangible:
-		anim = "Intangible"
-	sprite.play(anim)
->>>>>>> 6fdc375c240ba3e138241b592092c3d958f4f2e3
