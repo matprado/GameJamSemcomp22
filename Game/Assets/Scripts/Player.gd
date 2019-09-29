4bfc7751a69ec0271d2a51706d0024de1f1e0cc4
Tomieiro <matheustomieiro@usp.br>
2019-09-28 22:10:00 -0300

    Aqui

--- Game/Assets/Scripts/Player.gd
+++ Game/Assets/Scripts/Player.gd
@@ -5,10 +5,16 @@ onready var corpus = $Sprite
 onready var animation = $Sprite/anim
 var looking_direction = true
 var alt = 0
-var acceleration = 400;
+var acceleration = 400
+var up
+var down
+var left
+var right
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var verify = true
+	randomize_buttons()
 	pass # Replace with function body.
 
 #Fuction that swtiches body direction
@@ -20,17 +26,21 @@ func switch_dir():
 func _physics_process(delta):
 	var direction = 0
 
-	if Input.is_action_pressed("button_1"):
+	if Input.is_action_pressed(left):
 		direction = -1
-	if Input.is_action_pressed("button_2"):
+	if Input.is_action_pressed(right):
 		direction = 1
-    
+	
+	if Input.is_action_pressed(down):
+		randomize_buttons()
+		showWarning()
+	
 	move_and_slide(Vector2(acceleration*direction, alt), Vector2(0, -1))
    
 	alt += 50
 	
 	
-	if is_on_floor() and Input.is_action_just_pressed("button_3"):
+	if is_on_floor() and Input.is_action_just_pressed(up):
 		alt = -1000
 
 	if is_on_floor() and alt >= 5:
@@ -57,7 +67,29 @@ func _physics_process(delta):
 		animation.play("jump")
  
 
+func randomize_buttons():
+	var sample = [1,2,3,4]
+	var rn = []
+	var x
+	for i in range(4):
+		x = randi()%sample.size()
+		rn.append(sample[x])
+		sample.remove(x)
+	up = "button_" + str(rn.pop_front())
+	down = "button_" + str(rn.pop_front())
+	left = "button_" + str(rn.pop_front())
+	right = "button_" + str(rn.pop_front())
+
+func showWarning():
+	$warning.visible = true
+	$Timer.start()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	pass
+
+
+func _on_Timer_timeout():
+	$warning.visible = false;
+	pass # Replace with function body.
