08e001abfe460264dc6b20c641b6e0abebe2d774
Tomieiro <matheustomieiro@usp.br>
2019-09-28 22:16:21 -0300

    Final

--- Game/Assets/Scripts/BlockSM.gd
+++ Game/Assets/Scripts/BlockSM.gd
@@ -1,4 +1,4 @@
-extends Area
+extends Node2D
 
 
 onready var sprite = get_node("Sprite")
@@ -38,12 +38,3 @@ func ChangeState():
 	elif state == Slow:
 		anim = "Slow"
 	sprite.play(anim)
-
-
-func _on_BlockSM_body_entered(body):
-	if state == damage:
-		if body.has_method("die"):
-			body.die()
-	elif state == Slow:
-		if body.has_method("slow"):
-			body.slow()
